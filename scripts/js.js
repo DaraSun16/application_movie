6ddc90dff698dec61dd090bdbb03f7c5818e30c6
Dara <dara.tamako@gmail.com>
2025-06-03 16:25:18 +0200

    api response

--- scripts/js.js
+++ scripts/js.js
@@ -11,6 +11,30 @@ let getMovie = () => {
   } else {
     fetch(url).then((response) => response.json()).then((data) => {
       console.log(data);
+
+      if(data.Response === "True") {
+        result.innerHTML = 
+        `
+        <div class="info">
+          <div>
+            <h2>${data.Title}</h2>
+            <div class="rating">
+              <img src="imgs/star-icon.svg" alt="star">
+              <h4>${data.imdbRating}</h4>
+            </div>
+            <div class="details">
+              <span>${data.Rated}</span>
+              <span>${data.Year}</span>
+              <span>${data.Runtime}</span>
+            </div>
+          </div>
+        </div>
+        `
+      } else {
+        // si le film n'existe pas dans la base de données
+        result.innerHTML = `<h3 class="msg">${data.error} (le film n'existe pas)</h3>`;
+      }
+  
      }).catch(() => {
        result.innerHTML = `<h3 class="msg">Il y a eu une erreur.</h3>`;
     })
